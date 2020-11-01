c97bdd960ac2828889def82ba90f86914da01dff
yuvraj Pawar <yurajpawar99@gmail.com>
2020-11-01 22:11:56 +0530

    booking functionality has been added!

---
+++ app.js
@@ -0,0 +1,32 @@
+const express=require("express");
+const bodyParser = require("body-parser");
+const ejs=require("ejs");
+const app=express();
+
+app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+
+app.get("/",(req,res)=>{
+    res.sendFile("index.html");
+});
+
+app.get("/booking",(req,res)=>{
+res.render("booking");
+});
+
+
+
+app.post("/booking",(req,res)=>{
+    const body=req.body;
+    console.log(body);
+    res.render("successPage",{
+        message: "Your Reservation Has Been Made Successfully!",
+        path: "/"
+    });
+});
+
+
+app.listen(3000,()=>{
+    console.log("The hotel server is running!");
+});
